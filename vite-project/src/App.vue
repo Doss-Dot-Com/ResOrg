5fde48ffa49f06f651ed9dde995fc95c3dbfc14f
Doss-Dot-Com <ddoss0531@gmail.com>
2024-02-21 10:29:59 -0700

    OrgRes

---
+++ vite-project/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+      <div class="details-container">
+        <UserInfo
+        v-for="user in users"
+        :key="user.label"
+        :user-info="user"
+        />
+      </div>
+  </main>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import UserInfo from './components/UserInfo.vue'
+import "./style.css"
+
+const users = ref([
+  {label: "IP Address", value: "10.37.196.70"},
+  {label: "City", value: "Provo"},
+  {label: "Region", value: "Utah"},
+  {label: 'ZIP', value: '84606'},
+  {label: 'Country', value: 'United States'},
+  {label: 'School', value: 'Brigham Young University'},
+  {label: 'Name', value: 'John Doe'},
+  {label: 'Phone Number', value: '(555) 555-5555'},
+  {label: 'Email', value: 'X5k6Z@example.com'},
+  {label: 'Password', value: '********'},
+  {label: 'Username', value: 'johndoe'},
+  {label: 'Date of Birth', value: '01/01/2000'},
+  {label: 'Favorite Color', value: 'Blue'},
+  {label: 'Favorite Animal', value: 'Dog'},
+
+])
+
+</script>
---
+++ vite-project/src/components/UserInfo.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="grid-container">
+            <div class="info-item">
+                <span class="label">{{ userInfo.label }}</span>: <!-- Updated -->
+                <span class="value typingEffect">{{ userInfo.value }}</span> 
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    userInfo: Object
+})
+
+</script>
